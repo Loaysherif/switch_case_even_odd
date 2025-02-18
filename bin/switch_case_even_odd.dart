c15f3556abb33193f4554a2b07c56a03d225b977
Loay Sherif <loaysherif40@gmail.com>
2025-02-18 17:03:37 +0200

    final commit

--- bin/switch_case_even_odd.dart
+++ bin/switch_case_even_odd.dart
@@ -1,6 +1,9 @@
 void main(List<String> arguments) {
+// List of numbers from 1 to 10
   List<int> nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Loop through each number
   for (int i in nums) {
+// Check if the number is even or odd
     switch (i % 2) {
       case 0:
         print("$i is even");
--- test/switch_case_even_odd_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:switch_case_even_odd/switch_case_even_odd.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
